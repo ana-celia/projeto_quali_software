aa63b4461b63d9a79c18ade97d6791589f5aa3ae
ana-celia <araujo.acba@gmail.com>
2026-02-04 17:51:55 -0300

    fix: corrections following Flake8/PEP8

--- artesanato/api/serializers.py
+++ artesanato/api/serializers.py
@@ -1,28 +1,32 @@
 from rest_framework import serializers
-from .models import *
+from .models import Cliente, Endereco, Servico, Pedido, Relacao
+
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = "__all__"
 
+
 class EnderecoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Endereco
         fields = "__all__"
 
-class Servicoerializer(serializers.ModelSerializer):
+
+class ServicoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Servico
         fields = "__all__"
+
 
 class PedidoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pedido
         fields = "__all__"
 
+
 class RelacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Relacao
         fields = "__all__"
-
